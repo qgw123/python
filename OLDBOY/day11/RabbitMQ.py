a575d64d0722855448f7b480e11ee5a7bd4aa196
Administrator <admin@example.com>
2020-09-07 17:10:15 +0800

    commit

--- OLDBOY/day11/RabbitMQ.py
+++ OLDBOY/day11/RabbitMQ.py
@@ -5,6 +5,6 @@ Python
     threading QUEUE
     进程QUEUE
     父进程与子进程进行交互，或者同一个父进程下多个子进程进行交互
-    
+   
 '''
 
---
+++ OLDBOY/day11/consumer.py
@@ -0,0 +1,21 @@
+__author__ = 'qgw'
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+            'localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    # print('--->',ch, method, properties)
+    print('[x] Received %r' % body)
+
+channel.basic_consume(#消费消息
+                        'hello',
+                        callback,   #如果收到消息，就调用callback函数处理
+                       # queue='hello',
+                        True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
