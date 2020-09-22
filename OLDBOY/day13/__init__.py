f4aac1921aeac6d4c4bf3c3f34849e826d9de0a9
Administrator <admin@example.com>
2020-09-22 09:14:52 +0800

    commit

---
+++ OLDBOY/day13/__init__.py
@@ -0,0 +1,15 @@
+__author__ = 'qgw'
+
+'''
+完整示例代码
+https://github.com/triaquae/py3_training/tree/master/%E5%A0%A1%E5%9E%92%E6%9C%BA 
+'''
+
+'''
+堡垒机:
+    商业： 齐治
+    
+    开源： jumpserver
+           crazyeye
+           
+'''
